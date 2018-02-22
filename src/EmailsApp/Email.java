d8e052daadc73fcebfdc5581438c428758d3a969
Valerie <valerykyiv@gmail.com>
2018-02-22 09:00:19 +0100

    Version without password generation

--- src/EmailsApp/Email.java
+++ src/EmailsApp/Email.java
@@ -1,4 +1,72 @@
 package EmailsApp;
 
 public class Email {
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String department;
+    private String password;
+    private int capacity;
+    private String alternateEmail;
+
+    //setters
+    public void setPassword(String newPassword)
+    {
+        this.password = newPassword;
+    }
+    public void setFirstname(String newName)
+    {
+        this.firstname = newName;
+    }
+    public void setLastname(String newName)
+    {
+        this.lastname = newName;
+    }
+
+    public void setDepartment(int c)
+    {
+        String[] depArray = {"Sales" , "Development", "Accounting"};
+        for (int i = 0; i < depArray.length ; i++){
+            if (i == c){ this.department = depArray[i]; break;}
+        }
+    }
+
+    public void setCapacity(int newCapacity)
+    {
+        this.capacity = newCapacity;
+    }
+
+    public void setAlternateEmail(String newEmail)
+    {
+        this.alternateEmail = newEmail;
+    }
+
+    //getters
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public String getDepartment()
+    {
+        return this.department;
+    }
+
+    public String getName()
+    {
+        return this.firstname = this.firstname + " " + this.lastname;
+    }
+
+    public int getCapacity()
+    {
+        return this.capacity;
+    }
+
+    //generating
+    public String generateEmail()
+    {
+        String dep = getDepartment();
+        this.email = this.firstname + "." + this.lastname + "@" + dep + "." + "certicon.cz";
+        return email;
+    }
 }
--- src/EmailsApp/EmailApp.java
+++ src/EmailsApp/EmailApp.java
@@ -1,4 +1,36 @@
 package EmailsApp;
 
+import java.util.Scanner;
+import EmailsApp.Email;
+
 public class EmailApp {
+    public static void main (String [] args)
+    {
+        Email emailrecord = new Email();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Set first name: ");
+        String s = in.nextLine();
+        emailrecord.setFirstname(s);
+
+        System.out.println("Set last name: ");
+        s = in.nextLine();
+        emailrecord.setLastname(s);
+
+        System.out.println("Set password: ");
+        s = in.nextLine();
+        emailrecord.setPassword(s);
+        boolean flag = false;
+        while (flag == false){
+        System.out.println("Set department (0 - Sales,1 - Development,2 - Accounting): ");
+        int c = in.nextInt();
+        if (c > 2 ){System.out.println("Error! Must be from 0 to 2. Reenter again!");continue;}
+        else {flag = true; emailrecord.setDepartment(c);}
+        }
+        System.out.println("Set email capacity :");
+        int b = in.nextInt();
+        emailrecord.setCapacity(b);
+
+        System.out.println("Email will be: " + emailrecord.generateEmail());
+        System.out.println("Email capacity will be: " + emailrecord.getCapacity());
+    }
 }
